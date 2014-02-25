0242c7b3533804972efa7c2243261795f445e00c
Robert Simpson <ttfnrob@gmail.com>
2014-02-25 13:08:00 +0000

    Fix for cursor timeout issues on import
    Essentially on big imports an iterations the Db connection times out
    when it hits big blocks. Can be (poorly) fixed by disabling timeout
    when finding records
    -https://groups.google.com/forum/#!msg/mongomapper/84iDIzoLLR8/II5lZCDlU
    RUJ

--- script/import_classifications.rb
+++ script/import_classifications.rb
@@ -74,79 +74,81 @@ else
 	puts "...found records up to #{most_recent_date} - picking up from there."
 end
 
-pending_classifications = MongoClassification.where(:created_at.gte => Time.parse(most_recent_date.to_s)).sort(:created_at.asc)
-total = pending_classifications.size
+total = MongoClassification.collection.find({:created_at => { '$gte' => Time.parse(most_recent_date.to_s)}}).count()
 puts "#{total} of #{MongoClassification.size} classifications to process. Go!"
 
 counter = 0
-pending_classifications.each do |c|
-	counter+=1
-	puts "Processed #{counter}/#{total} classifications" if counter%1000==0 && counter>1
-	if c.try(:annotations)
-		@started_at = c.annotations.select{|a| a["started_at"] if a["started_at"]}.first["started_at"]
-		c.annotations.each do |a|
-			# begin
-				if a["type"]
+MongoClassification.collection.find({:created_at => { '$gte' => Time.parse(most_recent_date.to_s)}}, :sort => ['created_at', Mongo::ASCENDING], :timeout => false) do |cursor|
+    cursor.each do |cl|
+      	c = MongoClassification.load(cl)
+		counter+=1
+		puts "Processed #{counter}/#{total} classifications" if counter%1000==0 && counter>1
+		if c.try(:annotations)
+			@started_at = c.annotations.select{|a| a["started_at"] if a["started_at"]}.first["started_at"]
+			c.annotations.each do |a|
+				# begin
+					if a["type"]
 
-					person = Person.create({
-					  :x => a["coords"][0],
-					  :y => a["coords"][1],
-					  :page_id => c.zooniverse_id,
-					  :user_id => c.user_name,
-					  :first => a["note"]["first"],
-					  :surname => a["note"]["surname"],
-					  :rank => a["note"]["rank"],
-					  :reason => a["note"]["reason"],
-					  :created_at => @started_at
-					}) if a["type"] == "person" && a["note"]
+						person = Person.create({
+						  :x => a["coords"][0],
+						  :y => a["coords"][1],
+						  :page_id => c.zooniverse_id,
+						  :user_id => c.user_name,
+						  :first => a["note"]["first"],
+						  :surname => a["note"]["surname"],
+						  :rank => a["note"]["rank"],
+						  :reason => a["note"]["reason"],
+						  :created_at => @started_at
+						}) if a["type"] == "person" && a["note"]
 
-					place = Place.create({
-					  :x => a["coords"][0],
-					  :y => a["coords"][1],
-					  :page_id => c.zooniverse_id,
-					  :user_id => c.user_name,
-					  :typed_name => a["note"]["place"],
-					  :geocoded_name => a["note"]["name"],
-					  :lat => a["note"]["lat"],
-					  :lon => a["note"]["long"],
-					  :at_location => a["note"]["location"],
-					  :note => a["note"],
-					  :created_at => @started_at
-					}) if a["type"] == "place" && a["note"]
+						place = Place.create({
+						  :x => a["coords"][0],
+						  :y => a["coords"][1],
+						  :page_id => c.zooniverse_id,
+						  :user_id => c.user_name,
+						  :typed_name => a["note"]["place"],
+						  :geocoded_name => a["note"]["name"],
+						  :lat => a["note"]["lat"],
+						  :lon => a["note"]["long"],
+						  :at_location => a["note"]["location"],
+						  :note => a["note"],
+						  :created_at => @started_at
+						}) if a["type"] == "place" && a["note"]
 
-					diary_date = DiaryDate.create({
-					  :x => a["coords"][0],
-					  :y => a["coords"][1],
-					  :page_id => c.zooniverse_id,
-					  :user_id => c.user_name,
-					  :date => a["note"],
-					  :note => a["note"],
-					  :created_at => @started_at
-					}) if a["type"] == "diaryDate" && a["note"]
+						diary_date = DiaryDate.create({
+						  :x => a["coords"][0],
+						  :y => a["coords"][1],
+						  :page_id => c.zooniverse_id,
+						  :user_id => c.user_name,
+						  :date => a["note"],
+						  :note => a["note"],
+						  :created_at => @started_at
+						}) if a["type"] == "diaryDate" && a["note"]
 
-					activity = Activity.create({
-					  :x => a["coords"][0],
-					  :y => a["coords"][1],
-					  :page_id => c.zooniverse_id,
-					  :user_id => c.user_name,
-					  :category => a["note"],
-					  :created_at => @started_at
-					}) if a["type"] == "activity" && a["note"]
+						activity = Activity.create({
+						  :x => a["coords"][0],
+						  :y => a["coords"][1],
+						  :page_id => c.zooniverse_id,
+						  :user_id => c.user_name,
+						  :category => a["note"],
+						  :created_at => @started_at
+						}) if a["type"] == "activity" && a["note"]
 
-					weather = Weather.create({
-					  :x => a["coords"][0],
-					  :y => a["coords"][1],
-					  :page_id => c.zooniverse_id,
-					  :user_id => c.user_name,
-					  :category => a["note"],
-					  :created_at => @started_at
-					}) if a["type"] == "weather" && a["note"]
+						weather = Weather.create({
+						  :x => a["coords"][0],
+						  :y => a["coords"][1],
+						  :page_id => c.zooniverse_id,
+						  :user_id => c.user_name,
+						  :category => a["note"],
+						  :created_at => @started_at
+						}) if a["type"] == "weather" && a["note"]
 
-				end
-			# rescue
-			# 	puts "Error:"
-			# 	puts a
-			# end
+					end
+				# rescue
+				# 	puts "Error:"
+				# 	puts a
+				# end
+			end
 		end
 	end
 end
