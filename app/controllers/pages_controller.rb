4d427e6c57c46a7f1d0f74057b8ce8aabddb3715
Jim O'Donnell <jim@zooniverse.org>
2014-02-27 15:05:11 +0000

    prev/next page links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,8 @@ class PagesController < ApplicationController
   	@p = Page.find(params[:zoo_id])
   	@tags = @p.clusterize
   	@hex = {"diary_date" => "#ff0000", "person" => "#ff00ff", "place" => "#0000ff", "activity" => "#00ff00", "weather" => "#00ffff"}
+    @next_page = Page.find(:first, :conditions => ['page_number = ? and group_id = ?', @p.page_number + 1, @p.group_id])
+    @prev_page = Page.find(:first, :conditions => ['page_number = ? and group_id = ?', @p.page_number - 1, @p.group_id])
   end
 
 end
