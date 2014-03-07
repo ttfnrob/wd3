934f00cbad94a5353bcd7a3745c16081e6cd412b
Jim O'Donnell <jim@zooniverse.org>
2014-03-07 15:15:07 +0000

    set default clustering sensitivity to 5

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
 
   def show
   	@p = Page.find(params[:zoo_id])
-    n = params[:n] || 3
+    n = params[:n] || 5
   	@tags = @p.clusterize( n.to_i )
   	@hex = {"diaryDate" => "#ff0000", "person" => "#ff00ff", "place" => "#0000ff", "activity" => "#00ff00", "weather" => "#00ffff"}
     @next_page = Page.find(:first, :conditions => ['page_number = ? and group_id = ?', @p.page_number + 1, @p.group_id])
