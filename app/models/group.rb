4cfe4f9a5f64dbb2497772ec8b4548279e48b47b
Jim O'Donnell <jim@zooniverse.org>
2014-03-12 16:50:55 +0000

    percentage complete

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,8 +4,14 @@ class Group
   
   key :id, ObjectId
   key :zooniverse_id, String
+  key :stats, Hash
   
   def pages
     @pages ||= Subject.where('group.zooniverse_id' => self.zooniverse_id ).fields(:zooniverse_id, :location).sort('metadata.page_number').limit(20)
+  end
+  
+  def completed
+    completed = 100 * self.stats['complete'].to_f/self.stats['total'].to_f
+    completed.round unless self.stats['total'].to_i == 0
   end
 end
