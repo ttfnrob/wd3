ad45f2f1b0ffbe65db26bd3399e771e58e2ef036
Jim O'Donnell <jim@zooniverse.org>
2014-04-07 14:32:57 +0100

    script to populate the timeline collection

---
+++ script/build_timelines.rb
@@ -0,0 +1,20 @@
+counter = 0
+
+Group.each do |g|
+  counter += 1
+  puts "#{counter} #{g.zooniverse_id} #{g.name}"
+  
+  n = 5
+  threshold = 2
+  
+  timeline = []
+  
+  Timeline.sort(:page_number).find_each( :group => g.zooniverse_id ) do |t|
+    timeline << t
+  end
+  
+  if timeline.empty?
+    g.tags n.to_i, threshold.to_i
+    timeline = g.timeline
+  end
+end
