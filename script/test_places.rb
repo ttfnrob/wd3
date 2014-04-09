1ef635b5a8a32a828cc8b80787a64022c7302b4e
Jim O'Donnell <jim@zooniverse.org>
2014-04-09 16:49:54 +0100

    test script to find the best guess at geolocation for a place name

---
+++ script/test_places.rb
@@ -0,0 +1,27 @@
+place = ARGV[0] || 'Ypres'
+fields = ['name']
+votes = {}
+
+Tag.find_each( :type => 'place', :compare => place.upcase.gsub(/[^A-Z0-9]/, '') ) do |t|
+  
+  fields.each do |f|
+    votes[f] ||= {}
+    label = t['votes'][f]
+    #label = 'none' if label == ''
+    unless label == ['']
+      votes[ f ][ label ] ||= 0
+      votes[ f ][ label ] += 1
+    end
+  end
+  
+  puts t['label'], votes
+  
+end
+
+votes.each do |k,v|
+  max_vote = v.values.max
+  votes[k] = v.select { |k, vote| vote == max_vote }
+end
+
+puts '***********'
+puts votes
