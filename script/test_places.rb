9a3025c1c033f1531f1b737ff55cd92712eb67d3
Jim O'Donnell <jim@zooniverse.org>
2014-04-11 10:55:32 +0100

    Build Place records from the tagged data

--- script/test_places.rb
+++ script/test_places.rb
@@ -1,27 +1,61 @@
-place = ARGV[0] || 'Ypres'
-fields = ['name', 'lat', 'long', 'id']
-votes = {}
 
-Tag.find_each( :type => 'place', :compare => place.upcase.gsub(/[^A-Z0-9]/, '') ) do |t|
+fields = ['name', 'lat', 'long', 'id']
+place_names = []
+places = []
+count = 0
+
+Tag.fields( :label, :compare, :votes ).find_each( :type => 'place' ) do |p|
+  count += 1
+  places << {:label => p['label'], :compare => p['compare'], :votes => p['votes']} unless p['votes']['id'] == ''
+  puts count
+end
+
+places.collect{|t| {:label => t[:label], :compare => t[:compare]}}.uniq.each do |p|
+  place_names << p
+end
+
+puts place_names.length
+
+count = 0
+
+place_names.each do |place|
+  votes = {}
+  puts place[:compare]
   
-  fields.each do |f|
-    votes[f] ||= {}
-    label = t['votes'][f]
-    #label = 'none' if label == ''
-    unless label == ['']
-      votes[ f ][ label ] ||= 0
-      votes[ f ][ label ] += 1
+  places.select{|t| t[:compare] == place[:compare] }.each do |t|
+  
+    fields.each do |f|
+      votes[f] ||= {}
+      label = t[:votes][f]
+      #label = 'none' if label == ''
+      unless label == ['']
+        votes[ f ][ label ] ||= 0
+        votes[ f ][ label ] += 1
+      end
     end
+  
   end
   
-  puts t['label'], votes
+  max_vote = 0
+
+  votes.each do |k,v|
+    max_vote = v.values.max
+    votes[k] = v.select { |k, vote| vote == max_vote }
+  end
+  
+  puts "Vote #{max_vote}"
+
+  unless max_vote.nil? || max_vote < 3 || votes['name'].keys.length > 1
+    p = {
+      :label => place[:label],
+      :compare => place[:compare],
+      :name => votes['name'].keys.join(','),
+      :coords => [votes['long'].keys.join(',').to_f, votes['lat'].keys.join(',').to_f],
+      :id => votes['id'].keys.join(',').to_i
+    }
+    Place.new(p).save()
+    count += 1
+    puts "#{count} Created #{p}"
+  end
   
 end
-
-votes.each do |k,v|
-  max_vote = v.values.max
-  votes[k] = v.select { |k, vote| vote == max_vote }
-end
-
-puts '***********'
-puts votes
