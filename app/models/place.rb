6e19ed7eae01a85356b0eb1578b4e5182619aa7b
Jim O'Donnell <jim@zooniverse.org>
2014-04-11 10:54:50 +0100

    Place model

---
+++ app/models/place.rb
@@ -0,0 +1,14 @@
+class Place
+  include MongoMapper::Document
+  plugin GeoSpatial
+
+  set_collection_name "war_diary_places"
+
+  key :id, ObjectId
+  key :label, String
+  key :name, String
+  key :compare, String
+  key :id, Integer
+  geo_key :coords, Array
+
+end
