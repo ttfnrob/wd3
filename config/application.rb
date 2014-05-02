71c3cd8fd8f2322033ce35580d4a624c3b6349ec
Jim O'Donnell <jim@zooniverse.org>
2014-05-02 12:51:36 +0100

    Indexing

--- config/application.rb
+++ config/application.rb
@@ -85,6 +85,7 @@ module PageProcessor
         Timeline.ensure_index [[:page, 1]], :sparse => true
         Timeline.ensure_index [[:group, 1]], :sparse => true
         Timeline.ensure_index [[:page_number, 1]], :sparse => true
+        Timeline.ensure_index [[:page_order, 1]], :sparse => true
         Timeline.ensure_index [[:coords, '2d']], :sparse => true
         Timeline.ensure_index [[:datetime, 1]], :sparse => true
         Timeline.ensure_index [[:type, 1]], :sparse => true
